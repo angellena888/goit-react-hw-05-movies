329aaf51f804160b7858009abcd8b4f5d5190739
angellena888 <angellena888@gmail.com>
2023-05-25 19:50:38 +0300

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import FetchReviews from 'services/fetchReviews';
-import PropTypes from 'prop-types';
 
 const Reviews = () => {
   const { movieId } = useParams();
@@ -17,19 +16,17 @@ const Reviews = () => {
     <div>
       <ul>
         {review.map(review => {
-          <li key={review.id}>
-            <h3>{review.author}</h3>
-            <p>{review.content}</p>
-            <p>Rating: {review.author_details.rating}</p>
-          </li>;
+          return (
+            <li key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+              <p>Rating: {review.author_details.rating}</p>
+            </li>
+          );
         })}
       </ul>
     </div>
   );
-};
-
-Reviews.propTypes = {
-  movieId: PropTypes.string.isRequired,
 };
 
 export default Reviews;
